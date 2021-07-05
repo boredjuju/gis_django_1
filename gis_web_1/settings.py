a828da46df5343f9fc1ff0da2ea1e5f6c7d35d1c
Hyeonju Cha <hy9498@gmail.com>
2021-07-05 10:45:59 +0900

    7/5 gis django class commit 2

--- gis_web_1/settings.py
+++ gis_web_1/settings.py
@@ -135,7 +135,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# css file 경로 추가
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#
