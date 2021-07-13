417d16aa6c0e9f21607166ab561ab13b57cd8248
Hyeonju Cha <hy9498@gmail.com>
2021-07-13 14:42:12 +0900

    7/13 gis django class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 # def hello_world(request):
 #     return HttpResponse('Hello World!')
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 
@@ -17,11 +22,23 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # hello_world_list = HelloWorld.objects.all()
+        #
+        # return render(request, 'accountapp/hello_world.html',
+        #               context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': 'hello_world_list'})
+                      context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+
+
