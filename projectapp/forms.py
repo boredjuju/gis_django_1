dd54b506f3c73902588566b22b9f206d14926ac1
Hyeonju Cha <hy9498@gmail.com>
2021-08-17 14:43:03 +0900

    8/17 gis django class commit 1

---
+++ projectapp/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+
+from projectapp.models import Project
+
+
+class ProjectCreationForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = ['name', 'image', 'description']
+
+
+
+
+
+
+
+    
