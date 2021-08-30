2b8d3cf398fe13fcfd83d32134c085530756f47d
Hyeonju Cha <hy9498@gmail.com>
2021-08-30 10:12:05 +0900

    8/30 gis django class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def hello_world(request):
-#     return HttpResponse('Hello World!')
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
@@ -19,31 +17,10 @@ from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-@login_required(login_url=reverse_lazy('accountapp:login'))
-def hello_world(request):
-    # if request.user.is_authenticated:
-    if request.method == 'POST':
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        # hello_world_list = HelloWorld.objects.all()
-        #
-        # return render(request, 'accountapp/hello_world.html',
-        #               context={'hello_world_list': hello_world_list})
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
-    # else:
-    #     return HttpResponseRedirect(reverse('accountapp:login'))
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -82,6 +59,6 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
